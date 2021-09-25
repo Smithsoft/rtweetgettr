e2ea7a5c632738c0a25eaf91bc3fb4a6fae8b16a
Sarah Smith <sarah@smithsoft.com.au>
2021-09-25 15:37:49 +1000

    Switch to react native navigation
    Also refactor the main app view and tweetlist.

---
+++ src/Model/FakeData.ts
@@ -0,0 +1,33 @@
+const tw1: Tweet = {
+    id: 737517909140856833,
+    text: 'My tweet',
+    createdAt: new Date(),
+    name: 'Roger',
+    screenName: 'Roger ramjet',
+    userImage: 'placeholder',
+    profileImageURL: '',
+    biggerProfileImageURL: '',
+}
+const tw2: Tweet = {
+    id: 737517909140856834,
+    text: 'My tweet',
+    createdAt: new Date(),
+    name: 'Roger',
+    screenName: 'Wily coyote',
+    userImage: 'placeholder',
+    profileImageURL: '',
+    biggerProfileImageURL: '',
+}
+const tw3: Tweet = {
+    id: 737517909140856835,
+    text: 'My tweet',
+    createdAt: new Date(),
+    name: 'Roger',
+    screenName: 'Roger ramjet',
+    userImage: 'placeholder',
+    profileImageURL: '',
+    biggerProfileImageURL: '',
+}
+const tweets = [ tw1, tw2, tw3 ]
+
+export { tw1, tw2, tw3, tweets }
