afa2c2a1dfd2bb171d5c748a15b316d16fa0df8f
Sarah Smith <sarah@smithsoft.com.au>
2021-09-26 17:46:14 +1000

    Settings screen and user input

--- src/Model/TwitterClient.ts
+++ src/Model/TwitterClient.ts
@@ -1,10 +1,29 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-// An opaque unique JS symbol that behaves like string UUID
+import TwitterService from "../Service/TwitterService"
+import TwitterServiceClient from "../Service/TwitterServiceClient"
+import ErrorData from "../Types/ErrorData"
+import TweetDataResponse from "../Types/TweetDataResponse"
+import Tweet from "./Tweet"
 
+// An opaque unique JS symbol that behaves like string UUID
 // and can be used for weakly hiding computed property names
 const instanceName = Symbol()
 
-export class TwitterClient {
+type TweetResults = Tweet[] | ErrorData
+
+type TweetDataHandler = (results: TweetResults) => void
+
+class TwitterClient implements TwitterServiceClient {
+
+    twitter = new TwitterService(this)
+
+    /** If the user is logged in, contains the user's auth token */
+    token: string | null = null
+
+    /** The users user id number, if known; or null if not known */
+    userId: string | null = null
+
+    /** The users username if known, or null if not known */
+    userName: string | null = null
 
     // Singleton
     private static [instanceName]: TwitterClient | null = null
@@ -13,6 +32,56 @@ export class TwitterClient {
         if (!TwitterClient[instanceName]) {
             TwitterClient[instanceName] = new TwitterClient()
         }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         return TwitterClient[instanceName]!
     }
+
+    constructor() {
+        this.handleData = this.handleData.bind(this)
+        this.handleError = this.handleError.bind(this)
+        this.handleTokenExpiry = this.handleTokenExpiry.bind(this)
+    }
+
+    handleData(results: TweetDataResponse): void {
+        //
+    }
+
+    handleError(error: ErrorData): void {
+        //
+    }
+
+    handleTokenExpiry(): void {
+        this.logout()
+    }
+
+    logout(): void {
+        this.userId = null
+        this.userName = null
+        this.token = null
+    }
+
+    login(): void {
+        if (this.userName !== null) {
+            this.twitter.fetchUserDataByUserName(this.userName)
+        }
+    }
+
+    fetchTweetsForLoggedInUser(dataHandler: TweetDataHandler): void {
+        if (this.userId === null) {
+
+            // User id not current - attempt login
+            if (this.userName === null) {
+                // User login credentials not current - redirect to login
+                dataHandler({ title: "User not logged in", detail: "Log in to request tweets"})
+                return
+            }
+            
+            this.twitter.fetchUserDataByUserName(this.userName)
+            return
+        }
+
+        this.twitter.fetchTweetsForUser(this.userId)
+    }
 }
+
+export { TwitterClient }
